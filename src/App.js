a73c6816884be3fc53441f84994e130643df381d
hue <hue@wisa.co.kr>
2023-10-31 17:59:15 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { useState } from 'react';
 import './App.css';
-
+import './common_styles.css';
 
 function App() {
 
-  let [menuOn, setMenuOn] = useState('');
+  let [menuOn, setMenuOn] = useState(false);
+  let toggleMenu = () => {
+    setMenuOn(!menuOn);
+  };
 
   return (
     <div className="wrapper">
-      
       {/* header */}
       <header>
-        <div className='gnb wrap_inner2'>
+        <div className='gnb '>
           <h1 className='logo'>HyeonGyu</h1>
-          <div className={menuOn === 1 ? 'gnb_menu active' : 'gnb_menu'} onClick={ ()=>{ setMenuOn(1) } }>
+          <div className={`gnb_menu ${menuOn ? 'active' : ''}`} onClick={ toggleMenu }>
             <span></span>
             <span></span>
             <span></span>
@@ -25,9 +27,19 @@ function App() {
       </header>
       {/* cnt */}
       <section className='cnt_body'>
-        <section className='cnt_main'>main</section>
+        <section className='cnt_main'>
+          <div className='top_section tac'>
+            <p>Hello !</p>
+            <p>I'm Park HyeonGyu.</p>
+          </div>
+        </section>
       </section>
-      
+      <footer>
+        <div className='wrap_inner2'>
+          <p className='ft_info tac'>Designed and Developed by HyeonGyu Park</p>
+          <p className='copyright tac'>Copyright © 2023 HG</p>
+        </div>
+      </footer>
     </div>
   );
 }
